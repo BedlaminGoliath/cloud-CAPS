a547a07781fc5ed56709da3fe87687b606481ef8
christopher lopez <christopherwlopez89@gmail.com>
2023-02-11 15:33:53 -0800

    can queue messages in sqs

--- utils.js
+++ utils.js
@@ -10,7 +10,6 @@ const chance = new Chance();
 
 const QUEUES = {
     Pickup: "https://sqs.us-west-2.amazonaws.com/472017249091/CAPS-Hub-Pickup.fifo"
-
 }
 
 module.exports = { sqsClient, chance, QUEUES };
