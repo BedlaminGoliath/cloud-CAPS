93752a3a3bfcdde5765200bcefb901d4b1e97344
christopher lopez <christopherwlopez89@gmail.com>
2023-02-11 17:55:01 -0800

    got both driver and vendor up and running and queuing. tests incoming

--- driver/handler.js
+++ driver/handler.js
@@ -1,4 +1,48 @@
 const { 
-    ReceivedMessageCommand,
-    DeleteMEssageCommand,
-} = require("@aws-sdk.client-sqs");
+    ReceiveMessageCommand,
+    DeleteMessageCommand,
+} = require("@aws-sdk/client-sqs");
+const { sqsClient, QUEUES, chance } = require("../utils");
+
+function deliver(orderId){
+    console.log("Driver Finished the delivery", orderId);
+    handlePickup();
+}
+
+async function handlePickup(){
+    try { 
+        const recieved = await sqsClient.send(
+            new ReceiveMessageCommand({
+                QueueUrl: QUEUES.Pickup,
+            })
+        );
+        if(recieved.Messages?.length>0) {
+            await sqsClient.send(
+                new DeleteMessageCommand({
+                    QueueUrl: QUEUES.Pickup,
+                    ReceiptHandle: recieved.Messages[0].ReceiptHandle,
+                })
+            );
+        const payload = JSON.parse(recieved.Messages[0].Body);
+        console.log("driver recieved a pickup event!", payload);
+        setTimeout(
+            ()=>deliver(payload.orderId),
+            chance.integer({ min: 5000, max: 7000 })
+        );
+        } else {
+            console.log("No pickup ready");
+            setTimeout(handlePickup, 5000);
+        }
+    } catch (e) {
+        console.error("Failed to handlePickUp", e);
+    }
+}
+
+function startDriver(){
+    console.log("driver ready!");
+    handlePickup();
+}
+
+module.exports = {
+    startDriver,
+}
---
+++ tests/driver-handler.test.js
@@ -0,0 +1,4 @@
+const { QUEUES } = require("../utils");
+const { delivered, handlePickup } = require("../driver/handler");
+
+jest.use
